fca6798ba972cb586c5aeaf9ecbf1850b5c75e12
MikeHorn-git <123373126+MikeHorn-git@users.noreply.github.com>
2024-05-05 00:45:58 +0200

    Add files via upload

--- PsqlHunter.py
+++ PsqlHunter.py
@@ -1,31 +1,43 @@
+#!/usr/bin/env python3
+
+"""
+PsqlHunter: A tool to hunt SQL commands in pcap files.
+"""
+
 import argparse
 import csv
 import json
 import os
-import pyshark
 import re
+from urllib.parse import unquote
+import pyshark
 from tabulate import tabulate
 from tqdm import tqdm
-from urllib.parse import unquote
 
 def process_pcap(pcap):
+    """
+    Process a pcap file or a directory containing pcap files.
+    """
     if os.path.isfile(pcap):
         return {pcap: extract_sql(pcap)}
-    elif os.path.isdir(pcap):
+    if os.path.isdir(pcap):
         results_dict = {}
         for filename in os.listdir(pcap):
             if filename.endswith(".pcap") or filename.endswith(".pcapng"):
                 results_dict.update({filename: extract_sql(os.path.join(pcap, filename))})
         return results_dict
-    else:
-        print("[-] Invalid input")
-        return None
+    print("[-] Invalid input")
+    return None
 
 def extract_sql(pcap):
+    """
+    Extract SQL commands from a pcap file.
+    """
     cap = pyshark.FileCapture(pcap)
 
     sql_pattern = re.compile(
-        r'\b(SELECT|INSERT|UPDATE|DELETE|CREATE|DROP|ALTER|TRUNCATE|GRANT|REVOKE|EXEC|LOAD_FILE|OUTFILE|UNION)\b|\b(AND|OR)\b\s*[\s=!<>]|--|\b(SLEEP|WAITFOR|BENCHMARK)\b\(\s*',
+        r'\b(SELECT|INSERT|UPDATE|DELETE|CREATE|DROP|ALTER|TRUNCATE|GRANT|REVOKE|EXEC|'
+        r'LOAD_FILE|OUTFILE|UNION)\b|\b(AND|OR)\b\s*[\s=!<>]|--|\b(SLEEP|WAITFOR|BENCHMARK)\b\(\s*',
     )
 
     results = []
@@ -42,7 +54,10 @@ def extract_sql(pcap):
     return results
 
 def main():
-    parser = argparse.ArgumentParser(description='Hunt sql commands in pcap.')
+    """
+    Main function to handle command-line arguments and execute the script.
+    """
+    parser = argparse.ArgumentParser(description='Hunt SQL commands in pcap.')
     parser.add_argument('pcap', help='Path to the pcap file or folder containing pcap files')
     parser.add_argument('--csv', help='Export results to CSV', action='store_true')
     parser.add_argument('--json', help='Export results to JSON', action='store_true')
@@ -50,7 +65,7 @@ def main():
     args = parser.parse_args()
 
     results_dict = process_pcap(args.pcap)
-    
+
     if results_dict is None:
         print("[-] Invalid input: No pcap files found")
     elif not results_dict:
